379b1385e5397e84bf4ff0ac2ff1b0d295999a9a
djds4rce <daya@congnitiveclouds.com>
2014-07-05 23:51:48 +0530

    Fixed Linkden in Share

--- angular-socialshare.js
+++ angular-socialshare.js
@@ -52,41 +52,41 @@ angular.module('djds4rce.angular-socialshare', [])
     transclude: true,
     template: '<div class="facebookButton">' + 
       '<div class="pluginButton">' + 
-      '<div class="pluginButtonContainer">' + 
-      '<div class="pluginButtonImage">' + 
-      '<button type="button">' + 
-      '<i class="pluginButtonIcon img sp_plugin-button-2x sx_plugin-button-2x_favblue"></i>' + 
-      '</button>' + 
+        '<div class="pluginButtonContainer">' + 
+          '<div class="pluginButtonImage">' + 
+            '<button type="button">' + 
+              '<i class="pluginButtonIcon img sp_plugin-button-2x sx_plugin-button-2x_favblue"></i>' + 
+            '</button>' + 
+          '</div>' + 
+          '<span class="pluginButtonLabel">Share</span>' + 
+        '</div>' + 
       '</div>' + 
-      '<span class="pluginButtonLabel">Share</span>' + 
-      '</div>' + 
-      '</div>' + 
-      '</div>' + 
-      '<div class="facebookCount">' +
+    '</div>' + 
+    '<div class="facebookCount">' +
       '<div class="pluginCountButton pluginCountNum">' + 
-      '<span ng-transclude></span>' +
+        '<span ng-transclude></span>' +
       '</div>' + 
       '<div class="pluginCountButtonNub"><s></s><i></i></div>' + 
-      '</div>',
+    '</div>',
     link: function(scope, element, attr) {
       if(attr.shares){
         $http.get('https://api.facebook.com/method/links.getStats?urls='+attr.url+'&format=json').success(function(res){
-          var count = res[0].share_count.toString();
-          var decimal = '';
-          if(count.length > 6){
-            if(count.slice(-6,-5) != "0"){
-              decimal = '.'+count.slice(-6,-5);
+            var count = res[0].share_count.toString();
+            var decimal = '';
+            if(count.length > 6){
+              if(count.slice(-6,-5) != "0"){
+                decimal = '.'+count.slice(-6,-5);
+              }
+              count = count.slice(0, -6);
+              count = count + decimal + 'M';
+            }else if(count.length > 3){
+              if(count.slice(-3,-2) != "0"){
+                decimal = '.'+count.slice(-3,-2);
+              }
+              count = count.slice(0, -3);
+              count = count + decimal + 'k';
             }
-            count = count.slice(0, -6);
-            count = count + decimal + 'M';
-          }else if(count.length > 3){
-            if(count.slice(-3,-2) != "0"){
-              decimal = '.'+count.slice(-3,-2);
-            }
-            count = count.slice(0, -3);
-            count = count + decimal + 'k';
-          }
-          scope.shares = count;
+            scope.shares = count;
         }).error(function(){
           scope.shares = 0;
         });
@@ -119,7 +119,7 @@ angular.module('djds4rce.angular-socialshare', [])
       });
     }
   };
-}]).directive('linkdin', ['$timeout','$http', '$window',function($timeout,$http,$window) {
+}]).directive('linkedin', ['$timeout','$http', '$window',function($timeout,$http,$window) {
   return {
     scope: {
       shares: '=' 
@@ -127,22 +127,22 @@ angular.module('djds4rce.angular-socialshare', [])
     transclude: true,
     template: '<div class="linkedinButton">' + 
       '<div class="pluginButton">' + 
-      '<div class="pluginButtonContainer">' + 
-      '<div class="pluginButtonImage">in' + 
+        '<div class="pluginButtonContainer">' + 
+          '<div class="pluginButtonImage">in' + 
+          '</div>' + 
+          '<span class="pluginButtonLabel"><span>Share</span></span>' + 
+        '</div>' + 
       '</div>' + 
-      '<span class="pluginButtonLabel"><span>Share</span></span>' + 
-      '</div>' + 
-      '</div>' + 
-      '</div>' + 
-      '<div class="linkedinCount">' +
+    '</div>' + 
+    '<div class="linkedinCount">' +
       '<div class="pluginCountButton">' + 
-      '<div class="pluginCountButtonRight">' +
-      '<div class="pluginCountButtonLeft">' +
-      '<span ng-transclude></span>' +
-      '</div>' +
-      '</div>' +
+        '<div class="pluginCountButtonRight">' +
+          '<div class="pluginCountButtonLeft">' +
+            '<span ng-transclude></span>' +
+          '</div>' +
+        '</div>' +
       '</div>' + 
-      '</div>',
+    '</div>',
     link: function(scope, element, attr) {
       if(attr.shares){
         $http.jsonp('http://www.linkedin.com/countserv/count/share?url='+attr.link+'&callback=JSON_CALLBACK&format=jsonp').success(function(res){
